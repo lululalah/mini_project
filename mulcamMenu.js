63abb513b8b26399a2391ebc34c2095825b9fa7d
lululalah <hyojinlee1007@gmail.com>
2020-01-16 20:08:03 +0900

    Trying hover

--- mulcamMenu.js
+++ mulcamMenu.js
@@ -2,19 +2,32 @@
 	 
 	var menuObj = $("#menu");
 	$(".subMenu").hide();
-	$(".opt").hover(
+	$('.MenuOpt').wrap('<a href="#"></a>');
+	$(".MenuOpt").hover(
 			function(){
+				$(this).find('a').addClass('hover');
 				word = $(this).text();
 				word = $.trim(word);
-				$("#"+word).show();
+				$("#"+word).show("1000");
 				
-				var x = event.clientX; // 모든 event에 대해 처리
+				var x = event.clientX; // 좌표.모든 event에 대해 처리
 				var y = event.clientY+20;
 				$("#"+word).css( {"top":y,"left":x} );
-			},//function
-			function(){
-				$("#"+word).hide();				
-			}
+			},//function1
+			function(){	
+				$("#"+word).hide("1500");		
+			}//function2
+			
+			/*
+			 * function(){
+		var activeTab = $(this).attr("rel");
+		$("#"+activeTab).hide();
+		$("#"+activeTab).hover(function(){
+			$("#"+activeTab).show();
+                }, function(){
+			$("#"+activeTab).hide();
+		});	
+*/
 			);//hover
 	$(".img").hover(
 		function(){
@@ -31,6 +44,6 @@
 			},100);		
 			}
 	);
-	
-	
- });
+	$("#smartPick tr:eq(0)").contents().wrap('<a href="https://www.multicampus.com/system/menu/iframe?p_url=L3B1Ymxpc2gvcGFnZS9zbWFydHBpY2svcG9zdC9taWxsZW5uaWFsLmh0bWw=&p_menu=MTk5I01BSU4=&p_gubun=Qw=="></a>');
+/*//contents().wrap('<a href="#" class="menu1_link"></a>');*/	
+});//document function
